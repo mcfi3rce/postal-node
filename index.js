fc31491c003916f8e42b14e6c9a2d749aa0fac4b
adammcpherson <aomcpherson@gmail.com>
2018-11-15 23:09:59 -0600

    fixed error *maybe*

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var express = require('express'),
 // set the view engine to ejs
 app
     .use(express.static(path.join(__dirname, 'public'))),   
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .set('views', path.join(__dirname, 'views')),
+    .set('view engine', 'ejs'),
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // use res.render to load up an ejs view file
 
