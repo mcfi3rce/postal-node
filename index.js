e2644f196918d10d18242ac473f9564032c6de8d
adammcpherson <aomcpherson@gmail.com>
2018-11-23 19:52:54 -0600

    got database to connect

--- index.js
+++ index.js
@@ -12,8 +12,62 @@ app
     .set('view engine', 'ejs')
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-// use res.render to load up an ejs view file
+// Connect to the db
+const { Pool } = require("pg");
+const connectionString = process.env.DATABASE_URL || "postgres://mcfi3rce:safe@localhost:5432/booklife";
 
+// Establish the connection
+const pool = new Pool({connectionString: connectionString});
+
+// This function handles requests to the /books endpoint
+function getBooks(request, response) {
+	// This will be set to the user eventually
+    var id = 1;
+    
+	getBooksFromDb(id, function(error, result) {
+		// This is the callback function that will be called when the DB is done.
+		// The job here is just to send it back.
+
+		// Make sure we got a row with the person, then prepare JSON to send back
+		if (error || result == null) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			var person = result;
+			response.status(200).json(result);
+		}
+	});
+}
+
+// This function gets books
+function getBooksFromDb(id, callback) {
+	console.log("Getting books from DB");
+
+	// Set up the SQL that we will use for our query. Note that we can make
+	// use of parameter placeholders just like with PHP's PDO.
+	var sql = "SELECT * from public.book";
+    
+    // we will use this later for filtering
+    params = null;
+    
+	// This runs the query, and then calls the provided anonymous callback function
+	// with the results.
+	pool.query(sql, params, function(err, result) {
+		// If an error occurred...
+		if (err) {
+			console.log("Error in query: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		// Log this to the console for debugging purposes.
+		console.log("Found result: " + JSON.stringify(result.rows));
+
+		callback(null, result.rows);
+	});
+
+}  // end of getBooksFromDb
+
+// use res.render to load up an ejs view file
 // index page 
 app.get('/', function(req, res) {
     var drinks = [
@@ -35,7 +89,7 @@ app.get('/about', function(req, res) {
 });
 
 app.get('/books', function(req, res) {
-    res.render('pages/books');
+    getBooks(req, res);
 });
 
 
